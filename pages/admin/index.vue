19a86f60b95147407115c38a0ab95e2c323462f6
abhyss <abhijeetnikose@gmail.com>
2021-04-14 22:19:49 +0530

    initial commit

---
+++ pages/admin/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Admin home</h1>
+        <button @click="handleLogout">Logout</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'index',
+        methods: {
+            handleLogout() {
+                this.$store.dispatch('user/logout').then(() => this.$router.push('/'))
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ services/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/analytics';
+
+const config = {
+    apiKey: process.env.NUXT_ENV_FIREBASE_API_KEY,
+    authDomain: `${process.env.NUXT_ENV_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+    projectId: process.env.NUXT_ENV_FIREBASE_PROJECT_ID,
+    storageBucket: `${process.env.NUXT_ENV_FIREBASE_PROJECT_ID}.appspot.com`,
+    messagingSenderId: process.env.NUXT_ENV_FIREBASE_MSG_SENDER_ID,
+    appId: process.env.NUXT_ENV_FIREBASE_APP_ID,
+    measurementId: process.env.NUXT_ENV_FIREBASE_MEASUREMENT_ID
+};
+
+!firebase.apps.length && firebase.initializeApp(config);
+firebase.analytics();
+export const analytics = firebase.analytics();
+
+
+export const auth = firebase.auth();
